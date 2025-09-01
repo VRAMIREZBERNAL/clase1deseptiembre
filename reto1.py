6ff28c8026f7c3686fdf1df7eb55677cdd9daa7f
VRAMIREZBERNAL <164385771+VRAMIREZBERNAL@users.noreply.github.com>
2025-09-01 15:40:40 -0500

    reto 1

---
+++ reto1.py
@@ -0,0 +1,23 @@
+nivel_hemoglobina = float(input("Ingrese el nivel de su hemoglobina: "))
+genero = int(input("Ingrese su género (1 para masculino, 2 para femenino): "))
+if genero == 1:
+    if nivel_hemoglobina < 12.2:
+        print("Su nivel de hemoglobina es bajo.")
+    if nivel_hemoglobina == [12.2, 16.6]:
+        print("Su nivel de hemoglobina es normal.")
+    if nivel_hemoglobina > 16.6:
+        print("Su nivel de hemoglobina es alto.")
+        
+    
+    
+    
+if genero == 2:
+    if nivel_hemoglobina < 12.6:
+        print("Su nivel de hemoglobina es bajo.")
+    if nivel_hemoglobina == [12.6, 15.0]:
+        print("Su nivel de hemoglobina es normal.")
+    if nivel_hemoglobina > 15.0: 
+        print("Su nivel de hemoglobina es alto.")
+        
+else:
+    print("No es posible generar la alerta")
