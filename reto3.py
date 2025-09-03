8bd2e70b92423edb85c1c63f8eaba01ceb3340d5
VRAMIREZBERNAL <164385771+VRAMIREZBERNAL@users.noreply.github.com>
2025-09-02 21:11:35 -0500

    completado

--- reto3.py
+++ reto3.py
@@ -1,6 +1,10 @@
 n_pacientes = int(input("Ingrese el numero de pacientes: "))
 hombre_alto = hombre_normal = hombre_bajo = 0
 mujer_alto = mujer_normal = mujer_bajo = 0
+suma_hombre = 0
+suma_mujer = 0
+promedio_hombre = 0
+promedio_mujer = 0
 for i in range(n_pacientes):
     print(f"\nPaciente {i + 1}:")
     nivel_hemoglobina = float(input("Ingrese el nivel de su hemoglobina: "))
@@ -28,8 +32,28 @@ for i in range(n_pacientes):
         else:
             mujer_alto += 1
             diagnostico = "alto"
-    print(f"{nivel_hemoglobina}, {diagnostico}")
+    if genero == 1:
+        suma_hombre += nivel_hemoglobina
+    else:
+        suma_mujer += nivel_hemoglobina
+
+promedio_hombre = round(suma_hombre / (hombre_alto + hombre_normal + hombre_bajo),2)
+if promedio_hombre < 12.2:
+    alerta = "bajo"
+elif promedio_hombre >= 12.2 and promedio_hombre <= 16.6:
+    alerta = "normal" 
+else:
+    alerta = "alto"
     
+promedio_mujer = round(suma_mujer / (mujer_alto + mujer_normal + mujer_bajo),2)
+if promedio_mujer < 12.6:
+    alerta = "bajo"
+elif promedio_mujer >= 12.6 and promedio_mujer <= 15:
+    alerta = "normal"
+else:
+    alerta = "alto"
+print(f"Promedio de hemoglobina en hombres: {promedio_hombre}, {alerta}")
+print(f"Promedio de hemoglobina en mujeres: {promedio_mujer}, {alerta}")
 print(f"Nivel de hemoglobina alta: {hombre_alto}, {mujer_alto}")
 print(f"Nivel de hemoglobina bajo: {hombre_bajo}, {mujer_bajo}")
 print(f"Nivel de hemoglobina normal: {hombre_normal}, {mujer_normal}")
